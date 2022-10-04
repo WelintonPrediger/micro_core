37c7604500ebb16e9f30e7769296698243d347ed
wpsilva <wpsliva@maisaedu.com.br>
2022-10-04 13:38:34 -0300

    Adjust route name

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -8,7 +8,7 @@ class AppModule extends Module {
 
   @override
   final List<ModularRoute> routes = [
-    ModuleRoute(Modular.initialRoute, module: LoginModule()),
+    ModuleRoute('/', module: LoginModule()),
     ModuleRoute('/home', module: HomeModule()),
   ];
 
